ce6b0952ea4228a3495ad83e515aabeeff81e9b5
Brian Williams <heffergod@gmail.com>
2022-09-08 09:23:09 -0700

    added Joely's regex answer for further study

--- chal-balanced-parenthesis/balancedParens.js
+++ chal-balanced-parenthesis/balancedParens.js
@@ -24,3 +24,17 @@ const balancedParens = string => {
 }
 
 console.log(balancedParens(sample4))
+
+// bonus regex answer
+
+// function balancedParens(string){
+//     let opens = string.replace(/[^(]/g, "").length;
+//     let closes = string.replace(/[^)]/g, "").length;
+//     return opens === closes
+//   }
+  
+  // / // Expression Boundaries
+  // [ // Start Character Group
+  // ^ // Not operator in character group
+  // a // The character "a"
+  // ] // End character group
